8d984b41d3c017baeaf6cdd4f73df801f5fa3e57
karishma24-max <chauhankarishma824@gmail.com>
2022-11-09 18:45:04 +0530

    code added

--- front-end/src/pages/AllRoutes.jsx
+++ front-end/src/pages/AllRoutes.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
 import Home from './Home'
+import Publishing from './Publish/Publishing'
 function AllRoutes() {
   return <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/publish" element={<Publishing/>}/>
   </Routes>
 }
 
