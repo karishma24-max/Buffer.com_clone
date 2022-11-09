70a02d564374f533b0f697c1f9d9c256c489f1eb
Code-With-Hashim <hashim.khan83029@gmail.com>
2022-11-09 16:57:52 +0530

    added new file

--- front-end/src/pages/AllRoutes.jsx
+++ front-end/src/pages/AllRoutes.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
 import Home from './Home'
+import { MainPage_Template } from './Start-Page/Main-Page-template/Main-Page-template'
 function AllRoutes() {
   return <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/start-page" element={<MainPage_Template/>}/>
   </Routes>
 }
 
