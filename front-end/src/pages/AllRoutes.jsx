cba9a4a58d7d55ad982af0e6ca4a41101df04de7
Code-With-Hashim <hashim.khan83029@gmail.com>
2022-11-10 00:07:02 +0530

    added start page lil work

--- front-end/src/pages/AllRoutes.jsx
+++ front-end/src/pages/AllRoutes.jsx
@@ -1,12 +1,25 @@
+import { Switch } from '@chakra-ui/react'
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
 import Home from './Home'
-import { MainPage_Template } from './Start-Page/Main-Page-template/Main-Page-template'
+import { MainPage_Template, template_arr } from './Start-Page/Main-Page-template/Main-Page-template'
+
+
 function AllRoutes() {
-  return <Routes>
+  return (<Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/start-page" element={<MainPage_Template/>}/>
-  </Routes>
+    {
+      template_arr.map((i)=> {
+        if(i.file) {
+          return <Route path={`/start-page/${i.path}`} element={i.file} /> 
+        } else {
+          return <Route path={`/start-page/${i.path}`} element={<h1>No data is here</h1>} />
+        }
+      })
+    }
+
+  </Routes>)
 }
 
 export default AllRoutes
--- front-end/src/pages/Start-Page/Main-Page-template/Main-Page-template.jsx
+++ front-end/src/pages/Start-Page/Main-Page-template/Main-Page-template.jsx
@@ -1,35 +1,59 @@
 import { Box, Divider, Grid, GridItem, Heading, Text, Image } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom"
+import Template2 from "../../../Templates/Template2"
+import Template1 from "../../../Templates/Template1"
 import "./MainPage.module.css"
+import Editing from "../../Editing-Page/Edititng."
 
-const template_arr = [
+export const template_arr = [
     {
-        template: 'https://buffer-start-page.s3.amazonaws.com/presets/Default_X2.png',
-        title : 'Traveler'
-
+    id : 1,
+    template: "https://buffer-start-page.s3.amazonaws.com/presets/Default_X2.png",
+    title : "Traveler",
+    path : "Traveler-Template",
+    file : <Template1 />
+    
     },
     {
-        template: 'https://buffer-start-page.s3.amazonaws.com/presets/Flower_X2.png',
-        title : 'Flowers Shop'
+    template : "https://buffer-start-page.s3.amazonaws.com/presets/Flower_X2.png",
+    title : "Flowers Shop",
+    path : "Flowers-Shop-Template",
+    file : <Template2 />
     },
     {
-        template: 'https://buffer-start-page.s3.amazonaws.com/presets/Pizza_X2.png',
-        title : 'Pizza'
+    template : "https://buffer-start-page.s3.amazonaws.com/presets/Pizza_X2.png",
+    title : "Pizza",
+    path : "Pizza-Template"
     },
     {
-        template: 'https://buffer-start-page.s3.amazonaws.com/presets/Alexoo_X2.png',
-        title : 'Alexo'
+    template : "https://buffer-start-page.s3.amazonaws.com/presets/Alexoo_X2.png",
+    title : "Alexo",
+    path : "Alexo-Template"
     },
     {
-        template: 'https://buffer-start-page.s3.amazonaws.com/presets/Alba_X2.png',
-        title : 'Alba'
+    template : "https://buffer-start-page.s3.amazonaws.com/presets/Alba_X2.png",
+    title : "Alba",
+    path : "Alba-Template"
     },
     {
-        template: 'https://buffer-start-page.s3.amazonaws.com/presets/Novika_X2.png',
-        title : 'Novika'
+    template : "https://buffer-start-page.s3.amazonaws.com/presets/Novika_X2.png",
+    title : "Novika",
+    path : "Novika-Template"
     }
 ]
 
 export const MainPage_Template = () => {
+    const Navigate = useNavigate()
+
+    const handelTemplateNaviate = (path , id) => {
+        
+        console.log(id)
+        Navigate(`./${path}`)
+        
+    }
+    
+    
+
     return (
         <div>
             <div>
@@ -39,13 +63,13 @@ export const MainPage_Template = () => {
                 <Text fontSize={'md'} textAlign='center' fontFamily={'Montserrat'} m='1.5%'>
                     If you don't find a template that matches your brand you can customize it on the next step
                 </Text>
-                <Text fontFamily={"sans-serif"} fontSize={'md'} textAlign={'center'} m='auto' mt='7%' w='7%' borderBottom={'2px solid blue'}>All Template</Text>
+                <Text fontFamily={"sans-serif"} fontSize={'md'} textAlign={'center'} m='auto' mt='4%' w='7%' borderBottom={'2px solid blue'}>All Template</Text>
                 <Divider />
             </div>
             <Grid w='80%' m='auto' my='3%' templateColumns='repeat(3, 1fr)' gap={6}>
                 {
                     template_arr.map((i) => (
-                        <GridItem>
+                        <GridItem key={i.path} onClick={()=>handelTemplateNaviate(i.path , i.id)}>
                             <Box borderRadius={'10px'} bg={'gray.100'} >
                                 <Box w='70%' m='auto' >
                                     <Image pt='2em' src={i.template} />
