6fdeccee2ce469513cba6280a00183e607d733ed
Channa47 <103635080+Channa47@users.noreply.github.com>
2022-11-09 22:01:46 +0530

    signup-done

--- front-end/src/pages/AllRoutes.jsx
+++ front-end/src/pages/AllRoutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
+import Signup from './Authentication/Signup'
 import Home from './Home'
 import { MainPage_Template } from './Start-Page/Main-Page-template/Main-Page-template'
 function AllRoutes() {
   return <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/signup" element={<Signup/>}/>
     <Route path="/start-page" element={<MainPage_Template/>}/>
   </Routes>
 }
