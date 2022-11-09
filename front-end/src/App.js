25fc88d7b382ba4eca11044e32cf20415c399246
Karthik <karthikalakunta21@gmail.com>
2022-11-09 17:49:59 +0530

    added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import AllRoutes from './pages/AllRoutes';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <AllRoutes/>
-
+      <Footer/>
     </div>
   );
 }
