a144c007885aad3957faf5e6e225808fc00c1301
Karthik <karthikalakunta21@gmail.com>
2022-11-09 19:51:02 +0530

    modifyed

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const url = `mongodb+srv://mongo:<password>@cluster0.sbpkwdl.mongodb.net/Bluelock?retryWrites=true&w=majority`
-const connect = async()=>{
-    return mongoose.connect(url)
-}
-
-module.exports = connect;
---
+++ backend/src/fetures/users/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
