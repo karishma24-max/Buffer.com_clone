7514786bb74901ce759c139c34e8dff02af698bc
Code-With-Hashim <hashim.khan83029@gmail.com>
2022-11-10 23:14:26 +0530

    added json file

--- front-end/src/pages/AllRoutes.jsx
+++ front-end/src/pages/AllRoutes.jsx
@@ -1,28 +1,25 @@
 import { Switch } from '@chakra-ui/react'
 import React from 'react'
-import {Route,Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Signup from './Authentication/Signup'
 import Home from './Home'
 import { MainPage_Template, template_arr } from './Start-Page/Main-Page-template/Main-Page-template'
 
 import Publishing from './Publish/Publishing'
+import Template1 from '../Templates/Template1'
 
 function AllRoutes() {
   return (<Routes>
-    <Route path="/" element={<Home/>}/>
+    <Route path="/" element={<Home />} />
 
-    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/signup" element={<Signup />} />
 
-    <Route path="/publish" element={<Publishing/>}/>
+    <Route path="/publish" element={<Publishing />} />
 
-    <Route path="/start-page" element={<MainPage_Template/>}/>
+    <Route path="/start-page" element={<MainPage_Template />} />
     {
-      template_arr.map((i)=> {
-        if(i.file) {
-          return <Route key={i.path} path={`/start-page/${i.path}`} element={i.file} /> 
-        } else {
-          return <Route key={i.path} path={`/start-page/${i.path}`} element={<h1>No data is here</h1>} />
-        }
+      template_arr.map((i) => {
+        return <Route key={i.path} path={`/start-page/${i.path}`} element={<Template1 />} />
       })
     }
 
---
+++ front-end/src/redux/Template/template.type.js
@@ -0,0 +1,2 @@
+export const get_template = 'get/template-json'
+export const update_template = 'update/template.json'
