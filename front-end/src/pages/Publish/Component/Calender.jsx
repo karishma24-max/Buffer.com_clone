f72e07cd555b2c3768775ed3634e33c2aa58a189
karishma24-max <chauhankarishma824@gmail.com>
2022-11-12 05:32:23 +0530

    edit

--- front-end/src/pages/Publish/Component/Calender.jsx
+++ front-end/src/pages/Publish/Component/Calender.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react'
 
 import {ArrowBackIcon,ArrowForwardIcon } from '@chakra-ui/icons'
 import { background, Box, Button, Flex, Heading, Img, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Spacer, Text, Tooltip, useDisclosure } from '@chakra-ui/react'
-import styled from '@emotion/styled'
-import { GoNoNewline } from 'react-icons/go'
+
 import { AiFillFileImage } from 'react-icons/ai'
 import axios from 'axios'
+import Schedule from './Schedule'
 
 // import Days from 'react-calendar/src/MonthView/Days'
 const weekday=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
@@ -33,19 +33,23 @@ const getSorteddays=(month,year)=>{
 
 }
 
-const getdateobj=(day,month,year)=>{
-    return new Date (year,month,day)
-}
-
-const aredatethesame=(first,second)=>{
-    return first.getFullYear()===second.getFullYear()&& first.getMonth()===second.getMonth()&&first.getDate()===second.getDate()
-
-}
 
 
 
 
-const Calender = ({startingDate}) => {
+
+
+
+
+
+
+
+const Calender = ({startingDate,displaydata,userdisplay,setdata,arrdata}) => {
+
+
+  
+
+
     const [date,setDate]=useState(new Date())
    
    const [currentMonth,setcurrentMonth]=useState(startingDate.getMonth())
@@ -74,25 +78,33 @@ const nextmonth=()=>{
             setcurrentYear(prev=>prev-1)
         }
 }
-const [open,setopen]=useState(false)
+
 
     
-  const [image,setImage]=useState("")
+  
   let x= new Date()
+
   const current= x.getDate()
+  const current1= x.getMonth()
+  const current2= x.getFullYear()
   console.log(current)
-  const [userdata,setUserdata]=useState({img:"",text:""})
-  const handledata=(e)=>{
+  const [userdata,setUserdata]=useState({img:"",text:"",date:""})
+  
+  const handledata=(e,day)=>{
     const {value, name} = e.target
-        setUserdata({...userdata,[name]:value})
+    
+        setUserdata({...userdata,[name]:value,date:day})
 }
-const handleSubmit=()=>
+
+const handlesubmit=()=>
 {
-let response=axios.patch("  http://localhost:8080,userdata")
-console(response.data)
+let response=axios.post("http://localhost:8000/data",userdata)
+console.log(response.data)
+setdata()
 }
   return (
     <div><Flex mt="30px">
+    <img src="C:\fakepath\WhatsApp Image 2022-10-10 at 8.57.28 PM.jpeg" alt=""/>
   <Flex>  <Button ml="50px" onClick={prevmonth}><ArrowBackIcon/></Button><Heading size='md'>Today</Heading><Button onClick={nextmonth}><ArrowForwardIcon/>
     </Button> <Heading  size='md' ml="10px">{date.toLocaleString('en-US',{month:"long"})}{currentYear}</Heading></Flex>
     <Spacer />
@@ -106,34 +118,51 @@ console(response.data)
 <Tooltip  hasArrow  label='Create Post' bg="cornflowerblue"  placement='right'><div className='styledate' 
 style={{
          background: current > day ? 'rgb(237, 229, 229)': "white" ,
-         fontSize:current === day? "40px":"15px",
+        
          fontWeight:current===day?"bold":"none",
          color:current === day? "cornflowerblue":"black"
         }}
 
 
- onClick={onOpen}    ><Box w="50px" h="50px" >{day}</Box> 
+ onClick={onOpen}    ><Box w="50px" h="50px" >{day}{arrdata.map((ele)=><div >{ele.date == day? <Schedule text={ele.text} img={ele.img} id={ele.id} date={ele.date} month={current1} year={current2}/>:""}</div>)}
+ 
+  
+  
+  
+  
+  
+  
+  
+  </Box> 
     
   </div></Tooltip>)}</div> 
   </div>
-<Box>
-
-<Modal isOpen={isOpen} onClose={onClose} size="xl">
+ 
+  <Box>
+<Modal isOpen={isOpen} onClose={onClose} size="xl" bg="white">
         <ModalOverlay  />
         <ModalContent bg="white" height="500px">
           
           <ModalCloseButton color="grey" />
-         <ModalBody><Box border="2px solid lightgrey" mt={10} ml={10} mr={10} mb={10} height="420px">
-         <Flex><Box w="120px" color="blue" textAlign={"center"}><Input color="grey" border="2px dotted" mr="20px" mt="50px" type="file" name="img" onChange={handledata} / >
+         <ModalBody>
+         
+         
+         
+         <Box border="2px solid lightgrey" mt={10} ml={10} mr={10} mb={10} height="420px">
+         <Flex><Box w="120px" color="blue" textAlign={"center"}><Input type="file" color="grey" border="2px dotted" mr="20px" mt="50px"   name="img" onChange={handledata} / >
         <Box ml="35px" mt="40px"> <AiFillFileImage size="50px" color="rgb(91, 87, 87)"/>
-         </Box><Text ml="5px">select your file </Text></Box>
+         </Box>select your file 
+         <Input onChange ={handledata}  name="date" />
+
+          </Box>
 
         <Input placeholder="What would you like to share" name="text" onChange={handledata} w="70%" h="300px" />
 
          </Flex> </Box> <Button onClick={handlesubmit}>Submit</Button></ModalBody>
          </ModalContent>
           </Modal>
-</Box>
+  
+          </Box> 
     
 </div>
   )
--- front-end/src/pages/Publish/Component/Sidebar.jsx
+++ front-end/src/pages/Publish/Component/Sidebar.jsx
@@ -1,16 +1,27 @@
-import { Box, Button, Flex, Spacer, Text } from '@chakra-ui/react'
-import React from 'react'
-import {AiOutlineThunderbolt, AiTwotoneCalendar} from "react-icons/ai"
+import { Box, Button, Flex, Spacer, Text, Tooltip } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import {AiFillInstagram, AiOutlineThunderbolt, AiTwotoneCalendar} from "react-icons/ai"
 import {ChevronDownIcon} from "@chakra-ui/icons"
-const Sidebar = () => {
+import axios from 'axios'
 
+const Sidebar = ({displaydata,userdisplay}) => {
+  
   return (
     <Box w="280px" border="1px solid grey" ml="10px"  mt="30px"><Box w="170px" pr="20px" pl="10px" pt="10px" ml="10px"  h="50px" mt="30px" fontWeight="bold"  borderRadius="5px" color='blue' bg="rgb(91, 141, 234)" size='md' ><Flex><AiTwotoneCalendar pr="5px" pt="3px"/><Spacer/> Calender</Flex></Box>
+  
+ 
+
     <Flex w="200px" ml="10px" alignItems="center" w="170px"  fontWeight="bold"     h="50px" mt="30px" borderRadius="5px"  ><AiOutlineThunderbolt color="grey" ml="10px" size="20px"/> Campaign  <Spacer/>     <Box w="50px" h="30px" pt="7px" pl="14px"  bg="#E9D8FD" borderRadius="20px"><AiOutlineThunderbolt color="#9F7AEA"/></Box></Flex>
-    
-    <Box w="100px" fontWeight="bold" pl="10px"    h="50px" mt="30px" borderRadius="5px" b> <ChevronDownIcon/> Queue</Box>
+    <hr/>
+    <Box w="200px" fontWeight="bold" pl="10px"    h="50px" mt="30px" borderRadius="5px" b> <ChevronDownIcon/> Queue
+    <Box><Flex  mt="10px" bg="#E9D8FD" height="40px" width="170px" borderRadius="10px" pl="10px" alignItems={"center"}><AiFillInstagram/> {displaydata}<Button w="20px" h="20px" 
+    borderRadius="10px" bg="lightgreen" ml="60px">new</Button></Flex></Box></Box>
+
+
+
+
     </Box>
   )
 }
 
-export default Sidebar
+export default  Sidebar
